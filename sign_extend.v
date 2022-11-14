e2a43870ac43a2bc58780451d648ea4c351ce589
Prabhatatha <59072496+Prabhatatha@users.noreply.github.com>
2022-11-14 15:30:14 +0530

    Update sign_extend.v

--- sign_extend.v
+++ sign_extend.v
@@ -1,7 +1,8 @@
+`timescale 1ns / 1ps
 module sign_extend(in,out);
   input [15:0] in;
-  wire temp;
-  output reg [31:0] out;
+  wire[15:0] temp;
+  output  [31:0] out;
   assign temp={16{in[15]}};
   assign out={temp,in};
    endmodule
